7de208c068bcaf7b4ce46c5fd5e56abd3908e3d5
PVR <raseelapv@gmail.com>
2022-05-15 07:19:22 +0300

    2nd commit

--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+# def login(request):
+#     return HttpResponse('first app loaded in django')
+def login(request):
+    return render(request,'login.html')
+def home(request):
+    return render(request,'home.html')
+
